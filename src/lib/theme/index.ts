63fd12face153dcfe7e101364f54d12b55a38dd2
zoyth <f@cakemail.com>
2026-03-16 13:57:16 -0400

    Export all v2 theme utilities from package barrel
    generateDarkModeCSS, resolveTokens, getTailwindColors, migrateThemeV1toV2,
    and color math utilities now exported from @zoyth/simple-site-framework.

--- src/lib/theme/index.ts
+++ src/lib/theme/index.ts
@@ -1,6 +1,20 @@
 // ABOUTME: Theme utilities export
 // ABOUTME: Export all theme-related utilities
 
-export { generateThemeCSS, generateDesignTokens } from './generate-css';
+export { generateThemeCSS, generateDesignTokens, generateDarkModeCSS } from './generate-css';
 export { getFontConfig, getFontVariables } from './load-fonts';
 export { generateProseCSS, PROSE_CLASSES } from './prose';
+export { resolveTokens, type ResolvedTokens } from './resolve';
+export { getTailwindColors } from './tailwind';
+export { migrateThemeV1toV2 } from './migrate';
+export {
+  hexToRgb,
+  rgbToHex,
+  rgbToOklch,
+  oklchToRgb,
+  generateShadeRamp,
+  darken,
+  lighten,
+  type RGB,
+  type OKLCH,
+} from './color';
